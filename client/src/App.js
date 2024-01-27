c1bf3041e24dda30cc4b30e4b624305d67b9249c
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2024-01-27 15:15:53 +0800

    Cleanup default generated webapp

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [users, setUsers] = useState(['a', 'b', 'c', 'd']);
-  
-  useEffect(() => {
-    async function fetchUsers() {
-      const response = await fetch('/api');
-      const data = await response.json();
-      setUsers(data.users);
-    }
-
-    fetchUsers();
-  });
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ol>
-          {users.map(user => <li>{user}</li>)}
-        </ol>
-      </header>
+      Hello World
     </div>
   );
 }
