06b165c87b268fd49581390406e8ec9d54d0c073
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2024-01-27 18:07:07 +0800

    Add selecting of armor piece

--- client/src/components/AugmentPage/AugmentPage.js
+++ client/src/components/AugmentPage/AugmentPage.js
@@ -1,35 +1,45 @@
 import React, { useState } from 'react';
 
 function AugmentPage({setNames}) {
-    const [selectedSet, setSelectedState] = useState({ setDetails: null, augmentPool: [] });
+    const [armorSet, setArmorSet] = useState({ setDetails: null, augmentPool: [] });
+    const [armorPiece, setArmorPiece] = useState(null);
 
     async function fetchSelectedState(setName) {
-        if (!setNames.includes(setName) || setName === selectedSet.setDetails?.name) {
+        if (!setNames.includes(setName) || setName === armorSet.setDetails?.name) {
             return;
         }
         
         const setResponse = await fetch(`api/sets/${setName}`);
         const setDetails = await setResponse.json();
 
-        let augmentPool = selectedSet.augmentPool;
+        let augmentPool = armorSet.augmentPool;
 
-        if (augmentPool.length === 0 || selectedSet.setDetails.augPool !== setDetails.augPool) {
+        if (augmentPool.length === 0 || armorSet.setDetails.augPool !== setDetails.augPool) {
             const augmentResponse = await fetch(`api/augments/${setDetails.augPool}`);
             augmentPool = await augmentResponse.json();
         }
 
-        setSelectedState({ setDetails, augmentPool });
+        setArmorSet({ setDetails, augmentPool });
+        setArmorPiece(null);
+    }
+
+    function updateSelectedArmorPiece(pieceName) {
+        const armorPiece = armorSet.setDetails.pieces.find(piece => piece.name === pieceName);
+        setArmorPiece(armorPiece);
     }
 
     return (
         <div className="AugmentPage">
-            <input list="setNameList" onChange={(e) => fetchSelectedState(e.target.value)}/>
+            <input list="setNameList" onChange={(e) => fetchSelectedState(e.target.value)} placeholder="Choose an armor set"/>
             <datalist id="setNameList">
                 {setNames.map(setName => <option value={setName}/>)}
             </datalist>
+            <input list="armorPieceList" onChange={(e) => updateSelectedArmorPiece(e.target.value)} disabled={armorSet.setDetails === null} placeholder="Choose an armor piece"/>
+            <datalist id="armorPieceList">
+                {armorSet.setDetails?.pieces.map(piece => <option value={piece.name}/>)}
+            </datalist>
             <pre>
-                {JSON.stringify(selectedSet.setDetails, null, 2)}
-                {JSON.stringify(selectedSet.augmentPool, null, 2)}
+                {JSON.stringify(armorPiece, null, 2)}
             </pre>
         </div>
     );
