a670b225fbfda6b3b9b172f43dff1b82c2148c9c
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2024-01-28 21:54:44 +0800

    Improve responsive layout

--- client/src/components/ArmorPiecePanel/ArmorPiecePanel.js
+++ client/src/components/ArmorPiecePanel/ArmorPiecePanel.js
@@ -33,58 +33,59 @@ function ArmorPiecePanel({ armorPiece, resistanceChanges, setResistanceChanges, 
     const maxSlotChange = Array.from(decoString).reduce((sum, current) => sum + (4 - Number(current)), 0) + ((3 - decoString.length) * 4);
     return (
         <div className={styles.ArmorPiecePanel}>
-            <div className={styles.NameRow}>
-                <h3>{armorPiece?.name ?? '???'}</h3>
-            </div>
-            <div className={styles.SlotRow}>
-                <span className={styles.SlotLabel}>Slots</span>
-                <DecoSlotBlock decoString={armorPiece?.decos ?? ''}
-                    slotChange={slotChange}
-                    className={styles.SlotBlock}/>
-                <NumberDial value={slotChange}
-                    onChange={setSlotChange}
-                    min={0}
-                    max={maxSlotChange}
-                    disabled={!armorPiece} 
-                    className={styles.SlotDial}/>
-            </div>
-            <div className={styles.ResistanceTable}>
-                <ResistanceRow  resistanceKey="defense"
-                    armorPiece={armorPiece}
-                    resistanceChanges={resistanceChanges}
-                    setResistanceChanges={setResistanceChanges}
-                    label="Defense"/>
-                <ResistanceRow resistanceKey="fireRes"
-                    armorPiece={armorPiece}
-                    resistanceChanges={resistanceChanges}
-                    setResistanceChanges={setResistanceChanges}
-                    label="Fire Resist"/>
-                <ResistanceRow resistanceKey="waterRes"
-                    armorPiece={armorPiece}
-                    resistanceChanges={resistanceChanges}
-                    setResistanceChanges={setResistanceChanges}
-                    label="Water Resist"/>
-                <ResistanceRow resistanceKey="thunderRes"
-                    armorPiece={armorPiece}
-                    resistanceChanges={resistanceChanges}
-                    setResistanceChanges={setResistanceChanges}
-                    label="Thunder Resist"/>
-                <ResistanceRow resistanceKey="iceRes"
-                    armorPiece={armorPiece}
-                    resistanceChanges={resistanceChanges}
-                    setResistanceChanges={setResistanceChanges}
-                    label="Ice Resist"/>
-                <ResistanceRow resistanceKey="dragonRes"
-                    armorPiece={armorPiece}
-                    resistanceChanges={resistanceChanges}
-                    setResistanceChanges={setResistanceChanges}
-                    label="Dragon Resist"/>
-            </div>
-            <div className={styles.SkillsRow}>
-                Skills
-            </div>
-            <div className={styles.SkillList}>
-                {(armorPiece?.skills ?? []).map(skill => <div key={skill.name}>{skill.name} x{skill.level}</div>)}
+            <h2 className={styles.NameRow}>{armorPiece?.name ?? '???'}</h2>
+            <div className={styles.Panel}>
+                <section className={styles.ArmorStats}>
+                    <h3>Skills</h3>
+                    <div className={styles.SlotRow}>
+                        <span className={styles.SlotLabel}>Slots</span>
+                        <DecoSlotBlock decoString={armorPiece?.decos ?? ''}
+                            slotChange={slotChange}
+                            className={styles.SlotBlock}/>
+                        <NumberDial value={slotChange}
+                            onChange={setSlotChange}
+                            min={0}
+                            max={maxSlotChange}
+                            disabled={!armorPiece} 
+                            className={styles.SlotDial}/>
+                    </div>
+                    <ResistanceRow  resistanceKey="defense"
+                        armorPiece={armorPiece}
+                        resistanceChanges={resistanceChanges}
+                        setResistanceChanges={setResistanceChanges}
+                        label="Defense"/>
+                    <ResistanceRow resistanceKey="fireRes"
+                        armorPiece={armorPiece}
+                        resistanceChanges={resistanceChanges}
+                        setResistanceChanges={setResistanceChanges}
+                        label="Fire Resist"/>
+                    <ResistanceRow resistanceKey="waterRes"
+                        armorPiece={armorPiece}
+                        resistanceChanges={resistanceChanges}
+                        setResistanceChanges={setResistanceChanges}
+                        label="Water Resist"/>
+                    <ResistanceRow resistanceKey="thunderRes"
+                        armorPiece={armorPiece}
+                        resistanceChanges={resistanceChanges}
+                        setResistanceChanges={setResistanceChanges}
+                        label="Thunder Resist"/>
+                    <ResistanceRow resistanceKey="iceRes"
+                        armorPiece={armorPiece}
+                        resistanceChanges={resistanceChanges}
+                        setResistanceChanges={setResistanceChanges}
+                        label="Ice Resist"/>
+                    <ResistanceRow resistanceKey="dragonRes"
+                        armorPiece={armorPiece}
+                        resistanceChanges={resistanceChanges}
+                        setResistanceChanges={setResistanceChanges}
+                        label="Dragon Resist"/>
+                </section>
+                <section className={styles.ArmorSkills}>
+                    <h3>Skills</h3>
+                    <div className={styles.SkillList}>
+                        {(armorPiece?.skills ?? []).map(skill => <div key={skill.name}>{skill.name} x{skill.level}</div>)}
+                    </div>
+                </section>
             </div>
         </div>
     );
